9a3c85d628e4f8b70b6aaa39cb44d9eb4073ff1c
TakamitsuYotsuyanagi <yotsu12@jupiter.kanazawa-it.ac.jp>
2015-05-26 20:27:41 +0900

    【実装】Model(User)

--- cakeフォルダ/ranking/app/Model/User.php
+++ cakeフォルダ/ranking/app/Model/User.php
@@ -3,7 +3,22 @@
 class User extends AppModel{
 	
 	public $name = 'User';
-	public $order = 'User.score DESC';
 	
+	public $validate = array(
+		'name' => array(
+				'rule' => 'isUnique',
+				'required' => true,
+				'message' => '既に同じ名前のユーザが存在します。'
+			)
+	);
+
+	public $hasMany = array(
+		"Score" => array(
+			'className' => 'Score',
+			'condition' => '',
+			'order' => '',
+			'dependent'  => false,
+			'foreignKey' => 'user_id',
+		)
+	);
 }
-?>
