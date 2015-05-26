6f1984913fa3b9c58d7c32d14d0db08f89b97ab6
TakamitsuYotsuyanagi <yotsu12@jupiter.kanazawa-it.ac.jp>
2015-05-26 20:20:49 +0900

    【実装】ユーザ登録

--- cakeフォルダ/ranking/app/Controller/UsersController.php
+++ cakeフォルダ/ranking/app/Controller/UsersController.php
@@ -1,18 +1,23 @@
 <?php
+class UsersController extends AppController {
 
-class UsersController extends AppController{
-	
-	//使用するコントローラー名
 	public $name = 'User';
-	
-	public function index(){
-		
-		$data = $this->User->find('all');
-		$this->set('data',$data);
-		
-		
-	}
-	
-}
+	public $uses = array('Score','User');
 
-?>
+	public function index(){
+		$data = $this->Score->find('all');
+		$this->set('data',$data);
+	}
+	//ユーザ登録
+	public function add(){
+		$data = 0;
+		if($this->request->isPost()){
+			$record = $this->data;
+			$flg = $this->User->save($record);
+			if($flg){
+				$this->redirect('.');
+			}
+		}
+	}
+
+}
