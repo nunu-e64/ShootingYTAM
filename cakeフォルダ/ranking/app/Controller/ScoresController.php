3dacb019522d5a9655251c3d99d05b728e8c7879
TakamitsuYotsuyanagi <yotsu12@jupiter.kanazawa-it.ac.jp>
2015-05-28 22:19:04 +0900

    【実装】クライアントから得たスコアデータをDBへinsertする。（単純にUser関係なくinsert）

--- cakeフォルダ/ranking/app/Controller/ScoresController.php
+++ cakeフォルダ/ranking/app/Controller/ScoresController.php
@@ -8,9 +8,15 @@ class ScoresController extends AppController{
 	public function index(){
 		$this->autoRender = false;
 		$data = $this->Score->find('all');
-
 		echo json_encode($data);
 		 $this->set('data',$data);
+	}
+	//スコアの追加
+	public function add(){
+		$this->autoRender = false;
+		print_r($this->request->query['score']);
+		$record['Score']['score'] = $this->request->query['score'];
+		$this->Score->save($record);
 	}
 	
 }
