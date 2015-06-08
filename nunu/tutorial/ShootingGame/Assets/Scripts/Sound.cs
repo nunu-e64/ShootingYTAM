1edb753dcb506766621b7d1a1cdc7313ef655cb3
nunu-e64 <nunu-e64@users.noreply.github.com>
2015-06-08 19:24:32 +0900

    【改善】シーン遷移してもBGMが継続するように改善

---
+++ nunu/tutorial/ShootingGame/Assets/Scripts/Sound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour {
+
+	public bool dontDestroyEnabled = true;
+
+	private static Sound instance = null;
+
+	//public static Sound Instance {
+	//	get { return instance; }
+	//}
+	
+	void Awake () {
+		 if (instance != null && instance != this) {
+			 Destroy(this.gameObject);
+			 return;
+		 } else {
+			 instance = this;
+		 }
+		 DontDestroyOnLoad(this.gameObject);
+	}
+
+}
