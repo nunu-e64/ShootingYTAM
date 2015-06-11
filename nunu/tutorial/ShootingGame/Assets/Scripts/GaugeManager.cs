7ab95f28f8d859ba4835ec09e348b6767feab7fd
nunu-e64 <nunu-e64@users.noreply.github.com>
2015-06-12 00:13:18 +0900

    #4【実装】ゲージのカウントダウン表示【変更】ゲージ位置を画面上部に変更【変更】スコアのフォントを固定幅フォントに変更

--- nunu/tutorial/ShootingGame/Assets/Scripts/GaugeManager.cs
+++ nunu/tutorial/ShootingGame/Assets/Scripts/GaugeManager.cs
@@ -3,21 +3,57 @@ using System.Collections;
 
 /// <summary>
 /// ゲージの状態によって自機が弾を打てるかを切り替えるための関数群
-/// ・Animationから呼び出す
+/// ・AnimationEventから呼び出す
 /// </summary>
 public class GaugeManager : MonoBehaviour {
 
+	public float countDown = 0.00f;
+
 	private Spaceship playerSpaceShip = null;
+	private Animator animator;
+	private bool isShooting;
+
+	GUIText[] gaugeText;
+
+	void Start () {	
+		//GUITextの位置をゲージに合わせる
+		gaugeText = GetComponentsInChildren<GUIText> ();
+		foreach (GUIText item in gaugeText) {
+			item.transform.position = Camera.main.WorldToViewportPoint (transform.position);
+		}
+
+		animator = GetComponent<Animator>();
+	}
+
+
+	void Update () {
+		
+		if (!isShooting) {
+			string tmpText = countDown.ToString ("00.00");
+			gaugeText[0].text = tmpText;
+		}
+
+	}
 
 	public void SetPlayer(Spaceship ship) {
 		playerSpaceShip = ship;
 	}
 
-	public void StartPlayerShot() {
+	public void StartShooting () {
+		isShooting = true;
+		StartPlayerShot ();
+	}
+
+	public void StartCharging () {
+		isShooting = false;
+		StopPlayerShot ();
+	}
+
+	private void StartPlayerShot() {
 		if (playerSpaceShip) playerSpaceShip.shotable = true; else Debug.LogError(playerSpaceShip);
 		Debug.Log("StartPlayerShot");
 	}
-	public void StopPlayerShot() {
+	private void StopPlayerShot() {
 		if (playerSpaceShip) playerSpaceShip.shotable = false; else Debug.LogError(playerSpaceShip);
 		Debug.Log ("StopPlayerShot");
 	}
