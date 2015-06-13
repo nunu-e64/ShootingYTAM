59b8f56bc7fad9a2c6d45eaeaedf7526710f2a12
nunu-e64 <nunu-e64@users.noreply.github.com>
2015-06-13 21:54:46 +0900

    【改良】DestroyAreaをカメラサイズに一致

--- nunu/tutorial/ShootingGame/Assets/Scripts/DestroyArea.cs
+++ nunu/tutorial/ShootingGame/Assets/Scripts/DestroyArea.cs
@@ -2,9 +2,16 @@
 using System.Collections;
 
 /// <summary>
-/// 画面外に出た敵や弾の削除
+/// 画面外に出た敵や弾の削除を行う
 /// </summary>
 public class DestroyArea : MonoBehaviour {
+
+	void Start () {
+		//Scaleを画面に合わせる
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));		//画面右上のワールド座標取得
+		Vector2 scale = max * 2;											
+		transform.localScale = scale;
+	}
 
 	void OnTriggerExit2D (Collider2D c) {	//TIPS: 判定の有無はUnityのEdit->ProjectSetting->Physics2Dで設定
 		Destroy(c.gameObject);
