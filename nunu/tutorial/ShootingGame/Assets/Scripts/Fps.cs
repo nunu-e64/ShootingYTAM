4988c91f7c783d8c8a204d3afff16d8fc084cd84
nunu-e64 <nunu-e64@users.noreply.github.com>
2015-06-15 15:20:37 +0900

    #14【実装】FPS計測表示とデバッグ用ストップウォッチ

---
+++ nunu/tutorial/ShootingGame/Assets/Scripts/Fps.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Fps : MonoBehaviour {
+
+	float counter = 0;
+	const int FPS_CALC_INTERVAL = 60;
+
+	float fps;
+	float timer = 0;
+
+
+	// Update is called once per frame
+	void Update () {
+
+		++counter;
+		timer += Time.deltaTime;
+
+		if (counter >= FPS_CALC_INTERVAL) {
+			fps = FPS_CALC_INTERVAL/timer;
+			GetComponent<Text> ().text = fps.ToString ("00.00");
+			timer = 0;
+			counter = 0;
+		}
+
+	}
+}
---
+++ nunu/tutorial/ShootingGame/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TimeManager {
+
+	static bool isRunning= false;		//時間計測フラグ
+	static int startTime = 0;
+
+	public static void StartCount(){
+		isRunning = true;
+		startTime = System.Environment.TickCount;
+	}
+
+	public static int FinishCount () {
+		if (isRunning) {
+			return System.Environment.TickCount - startTime;
+		} else{
+			Debug.LogWarning ("TimerManager.FinishCount called but not started.");
+			return 0;
+		}
+	}
+}
