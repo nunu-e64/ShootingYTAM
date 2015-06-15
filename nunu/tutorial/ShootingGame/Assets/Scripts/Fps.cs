5bf655e763b2725c54279b2102e7c94efe6c8474
nunu-e64 <nunu-e64@users.noreply.github.com>
2015-06-15 15:53:09 +0900

    #14【整理】コメント付記と不要な変数削除

--- nunu/tutorial/ShootingGame/Assets/Scripts/Fps.cs
+++ nunu/tutorial/ShootingGame/Assets/Scripts/Fps.cs
@@ -4,21 +4,19 @@ using System.Collections;
 
 public class Fps : MonoBehaviour {
 
-	float counter = 0;
-	const float FPS_CALC_INTERVAL = 1.0f;
+	const float FPS_CALC_INTERVAL = 1.0f;	//何秒単位でFPS表示を更新するか
 
-	float fps;
-	float timer = 0;
+	float counter = 0;	//フレームカウンタ
+	float timer = 0;	//実時間カウンタ
 
 
-	// Update is called once per frame
 	void Update () {
 
 		++counter;
 		timer += Time.deltaTime;
 
 		if (timer >= FPS_CALC_INTERVAL) {
-			fps = counter/timer;
+			float fps = counter/timer;
 			GetComponent<Text> ().text = fps.ToString ("00.00");
 			timer = 0;
 			counter = 0;
--- nunu/tutorial/ShootingGame/Assets/Scripts/TimeManager.cs
+++ nunu/tutorial/ShootingGame/Assets/Scripts/TimeManager.cs
@@ -6,11 +6,13 @@ public static class TimeManager {
 	static bool isRunning= false;		//時間計測フラグ
 	static int startTime = 0;
 
+	//時間計測開始
 	public static void StartCount(){
 		isRunning = true;
 		startTime = System.Environment.TickCount;
 	}
 
+	//時間計測終了してstartTimeとの差分を返す。初期化はしないので繰り返し呼ぶことが可能。
 	public static int FinishCount () {
 		if (isRunning) {
 			return System.Environment.TickCount - startTime;
